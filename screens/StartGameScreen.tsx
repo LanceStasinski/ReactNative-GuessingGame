fb5da6d6a5bf6ed437dd02e9f8ff2a062849eea1
LanceStasinski <lance.stasinski@gmail.com>
2022-05-05 10:32:01 -0400

    style: Style PrimaryButton

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -12,8 +12,14 @@ const StartGameScreen: FC = () => {
         autoCapitalize="none" //useful for things like emails
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -22,11 +28,13 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     elevation: 4, //creates shadow on Android
     shadowColor: "black", // shadow properties used by iOS
     shadowOffset: { width: 0, height: 2 },
@@ -43,5 +51,11 @@ const styles = StyleSheet.create({
     color: "#ddb52f",
     marginVertical: 8,
     fontWeight: "bold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
